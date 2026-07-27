2d07ca752c980b30cf815c10cd4366ff9f3fd67c
vishnugudeli7 <vishnugudeli7@gmail.com>
2026-07-27 12:39:50 +0530

    Update fix.sql

--- scripts/silver/fix.sql
+++ scripts/silver/fix.sql
@@ -29,3 +29,35 @@ select *,ROW_NUMBER() over(partition by cst_id
 order by cst_create_date desc) as flag_last
 from bronze.crm_cust_info) as t
 where flag_last = 1 and cst_id is not null;
+
+
+
+
+
+INSERT INTO silver.crm_prd_info(
+cat_id,
+prd_key,
+prd_id,
+prd_nm,
+prd_cost,
+prd_line,
+prd_start_dt,
+PRD_END_dt
+)
+SELECT 
+REPLACE( SUBSTRING(prd_key,1,5),'-','_') as cat_id,
+SUBSTRING(prd_key,7,len(prd_key)) as prd_key,
+prd_id,
+prd_nm,
+ISNULL(prd_cost,0 ) as prd_cost,
+CASE
+ when UPPER(TRIM(prd_line)) = 'M' THEN 'Mountain'
+ when UPPER(TRIM(prd_line)) = 'R' THEN 'Road'
+ when UPPER(TRIM(prd_line)) = 'S' THEN 'Other Sales'
+ when UPPER(TRIM(prd_line)) = 'T' THEN 'Touring'
+ else 'n/a'
+END AS prd_line,
+cast(prd_start_dt as date) as prd_start_dt,
+CAST(lead(prd_start_dt) OVER(PARTITION BY prd_key order by prd_start_dt) - 1 as DATE)
+AS PRD_END_dt
+FROM bronze.crm_prd_info;
